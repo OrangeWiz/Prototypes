447233ea2c075fbe39487bb4a6c61b1f473e7584
OrangeWiz <gopi.rayini@gmail.com>
2021-02-03 16:49:24 -0500

    Tree Move script

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRight = 10f;
+    public float changetoChangeDirection;
+    public float secondsBetweenAppleDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Change Directions
+        if (pos.x < -leftAndRight)
+        {
+            speed = Mathf.Abs(speed);
+
+        } else if (pos.x > leftAndRight)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(Random.value < changetoChangeDirection)
+        {
+            speed *= -1;
+        }
+    }
+}
