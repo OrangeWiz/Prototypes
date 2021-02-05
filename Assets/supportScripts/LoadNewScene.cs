d7bc68f0ac06fff38cd5ce925ea9acd5a516ed43
OrangeWiz <gopi.rayini@gmail.com>
2021-02-04 20:00:06 -0500

    Integration with Main Scene

--- Assets/supportScripts/LoadNewScene.cs
+++ Assets/supportScripts/LoadNewScene.cs
@@ -8,7 +8,7 @@ public class LoadNewScene : MonoBehaviour
 
     public void LoadScene1()
     {
-        SceneManager.LoadScene("Main-ApplePicker");
+        SceneManager.LoadScene("ApplePicker");
     }
 
     public void LoadScene2()
